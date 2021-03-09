8981d9c46ced7a8c95fe293ce0fa7d4eb1ddd1ad
MaiLinkKute <80353518+MaiLinkKute@users.noreply.github.com>
2021-03-10 01:16:43 +0700

    Create b12.py

---
+++ b12.py
@@ -0,0 +1,8 @@
+h= input ()
+if h[0].isupper() == True : right = 1 ; wrong = 0
+else : wrong = 1 ; right = 0
+for c in range(len(h)) :
+	if h[c] == " " : 
+		if h[c+1].isupper() == True : right+=1
+		else : wrong +=1
+print ("right : " ,right , "wrong : " ,wrong
